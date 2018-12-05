c26d780cc1fc3712386c9af589af95cfae83284b
User <User@YD03-15>
2018-12-05 16:47:49 +0900

    ss

--- DDosProject/src/main/java/com/ddos/web/login/LoginController.java
+++ DDosProject/src/main/java/com/ddos/web/login/LoginController.java
@@ -30,12 +30,10 @@ public class LoginController {
 		
 	}
 	
-<<<<<<< HEAD
 	@RequestMapping("/read")
 	public String read() {
 		return "read/ReadChoose";
 	}
-=======
 	
 	@RequestMapping("/booksss")
 	public String book() {
@@ -44,7 +42,6 @@ public class LoginController {
 		
 	}
 	
->>>>>>> branch 'master' of https://github.com/leeyeongmin/DDos.git
 
 	/*@RequestMapping("login")
 	public String login(@ModelAttribute("user") UserVO vo, HttpSession session) { // UserVO 를 jsp에서 user로 사용

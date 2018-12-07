3714a867c7d2a16aa6222aea55ba05675df23cdf
User <User@User-PC>
2018-12-07 17:38:48 +0900

    도서 수정

--- DDosProject/src/main/java/com/ddos/web/book/impl/BookServiceImpl.java
+++ DDosProject/src/main/java/com/ddos/web/book/impl/BookServiceImpl.java
@@ -11,24 +11,9 @@ import com.ddos.web.book.BookVO;
 @Service
 public class BookServiceImpl implements BookService {
 
-<<<<<<< HEAD
-	@Autowired
-	BookDAOMybatis dab;
-=======
-	@Override
-	public List<BookVO> getBookList(BookVO vo) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public BookVO getBook(BookVO vo) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	@Autowired BookDAOMybatis dab;
 
-	/*@Autowired BookDAOMybatis dab;
->>>>>>> branch 'master' of https://github.com/leeyeongmin/DDos.git
 
 	// 전체 조회
 	@Override
@@ -40,7 +25,7 @@ public class BookServiceImpl implements BookService {
 	@Override
 	public BookVO getBook(BookVO vo) {
 		return dab.getBook(vo);
-<<<<<<< HEAD
+
 	}
 
 	// 도서 등록
@@ -48,11 +33,11 @@ public class BookServiceImpl implements BookService {
 		dab.insertBook(vo);
 	}
 	// 도서 수정
-=======
-	}*/
-	
-	
-	
->>>>>>> branch 'master' of https://github.com/leeyeongmin/DDos.git
 
-}
+	}
+	
+	
+	
+
+
+

68c20d67edeccda79e0a2541f46b0524d7da9106
User <User@YD03-12>
2018-12-19 17:47:43 +0900

    리뷰 수정

--- DDosProject/src/main/java/com/ddos/web/book/ctrl/BookController.java
+++ DDosProject/src/main/java/com/ddos/web/book/ctrl/BookController.java
@@ -44,19 +44,18 @@ public class BookController {
 	// 도서 1건 조회 및 isbn으로 리뷰 조회
 	@RequestMapping("getBook")
 	public String getBook(Model model, BookVO vo, ReviewVO rvo) {
-		
+
 		model.addAttribute("book", bookservice.getBook(vo));
 		model.addAttribute("review", reviewservice.getReviewList(rvo));
-		
+
 		return "book/getBook";
-		
-/*		System.out.println(vo);
-	ReviewVO ro = new ReviewVO();
-	ro.setIsbn(vo.getIsbn());
-	ro.setPageUnit(3);
-		model.addAttribute("book", bookservice.getBook(vo));
-		model.addAttribute("review", reviewservice.getReviewList(ro));
-		return "book/getBook";	*/
+
+		/*
+		 * System.out.println(vo); ReviewVO ro = new ReviewVO();
+		 * ro.setIsbn(vo.getIsbn()); ro.setPageUnit(3); model.addAttribute("book",
+		 * bookservice.getBook(vo)); model.addAttribute("review",
+		 * reviewservice.getReviewList(ro)); return "book/getBook";
+		 */
 	}
 
 	// 도서 등록 폼
@@ -72,93 +71,81 @@ public class BookController {
 		return "redirect:getBookList";
 	}
 
-// 도서 수정 폼
+	// 도서 수정 폼
 	@RequestMapping("updateBookform")
 	public String updateBookform(Model model, BookVO vo) {
 		model.addAttribute("book", bookservice.getBook(vo));
 		return "book/updateBook";
 	}
 
-	//도서 수정 처리
+	// 도서 수정 처리
 	@RequestMapping("/updateBook")
 	public String updateBook(BookVO vo) {
 		bookservice.updateBook(vo);
 		return "redirect:getBookList";
 	}
-////////////////////////////////////// 리뷰  컨트롤러 /////////////////////////////////////////////////
-	
-	
-	// 리뷰 조회
-		@RequestMapping("getReviewList")
-		public List<ReviewVO> getReviewList(ReviewVO rvo){
-			return reviewservice.getReviewList(rvo);
-		}
+	////////////////////////////////////// 리뷰 컨트롤러
+	////////////////////////////////////// /////////////////////////////////////////////////
 
-		
-		//리뷰 단권 조회
-		@RequestMapping("getReview")
-		public String getReview(Model model, ReviewVO rvo) {
-			model.addAttribute("review", reviewservice.getReview(rvo));
-			return "book/getBook";
-					
-		}
-		// 리뷰 등록 폼
-		@RequestMapping("insertReviewform")
-		public String insertReviewform(Model model, BookVO vo, ReviewVO rvo) {
-			System.out.println(rvo);
-			model.addAttribute("review", rvo.getIsbn());
-			model.addAttribute("book", bookservice.getBook(vo));
-			System.out.println("리뷰 컨트롤 등록 폼");
-		return "book/insertReview";
-			
-		
-		}
-		// 리뷰 등록 처리
-		@RequestMapping("insertReview")
-			public String insertReview(Model model, BookVO vo, ReviewVO rvo, HttpSession session) {
-			System.out.println("리뷰 컨트롤 등록 처리");
-			reviewservice.insertReview(rvo);			
-			vo.setIsbn(rvo.getIsbn());
-			model.addAttribute("book", bookservice.getBook(vo));
-			model.addAttribute("review", reviewservice.getReviewList(rvo));
-			
-			return "book/getBook";
-			//return "redirect:getBook";
-		}
-		// 리뷰 수정 폼
-		@RequestMapping("updateReviewform")
-		public String updateReview(Model model, BookVO vo, ReviewVO rvo) {
-			System.out.println(rvo);
-			model.addAttribute("review", rvo.getIsbn());
-			model.addAttribute("book", reviewservice.getReviewList(rvo));
-			System.out.println("리뷰 수정 컨트롤 폼");
-			System.out.println(rvo);
-			return "book/updateReview";
-		}
-		
-		//리뷰 수정 처리
-		@RequestMapping("/updateReview")
-		public String updateReview(Model model, BookVO vo, ReviewVO rvo,  HttpSession session) {
-			System.out.println("리뷰 컨트롤 수정 처리");
-			reviewservice.updateReview(rvo);
-			vo.setIsbn(rvo.getIsbn());
-			model.addAttribute("book", bookservice.getBook(vo));
-			model.addAttribute("review", reviewservice.getReviewList(rvo));
-			return "redirect:getBook";
-		}
-		
-		// 리뷰 삭제
-		@RequestMapping("deleteReview")
-		public String deleteReview(Model model, ReviewVO rvo) {
-			
-			System.out.println(rvo);
-			
-			reviewservice.deleteReview(rvo);
-			BookVO vo = new BookVO();
-			vo.setIsbn(rvo.getIsbn());
-			model.addAttribute("review", reviewservice.getReviewList(rvo));
-			return "redirect:getBook";
-		}
-		
-		
+	// 리뷰 조회
+	@RequestMapping("getReviewList")
+	public List<ReviewVO> getReviewList(ReviewVO rvo) {
+		return reviewservice.getReviewList(rvo);
 	}
+
+	// 리뷰 단권 조회
+	@RequestMapping("getReview")
+	public String getReview(Model model, ReviewVO rvo) {
+		model.addAttribute("review", reviewservice.getReview(rvo));
+		return "book/getBook";
+	}
+
+	// 리뷰 등록 폼
+	@RequestMapping("insertReviewform")
+	public String insertReviewform(Model model, BookVO vo, ReviewVO rvo) {
+		System.out.println(rvo);
+		model.addAttribute("review", rvo.getIsbn());
+		model.addAttribute("book", bookservice.getBook(vo));
+		System.out.println("리뷰 컨트롤 등록 폼");
+		return "book/insertReview";
+
+	}
+
+	// 리뷰 등록 처리
+	@RequestMapping("insertReview")
+	public String insertReview(Model model, BookVO vo, ReviewVO rvo, HttpSession session) {
+		System.out.println("리뷰 컨트롤 등록 처리");
+		reviewservice.insertReview(rvo);
+		vo.setIsbn(rvo.getIsbn());
+		model.addAttribute("book", bookservice.getBook(vo));
+		model.addAttribute("review", reviewservice.getReviewList(rvo));
+
+		return "book/getBook";
+		// return "redirect:getBook";
+	}
+
+	// 리뷰 수정 폼
+	@RequestMapping("updateReviewform")
+	public String updateReview(Model model, ReviewVO rvo) {
+		System.out.println(rvo);
+		model.addAttribute("review", reviewservice.getReview(rvo));
+		return "book/updateReview";
+	}
+
+	// 리뷰 수정 처리
+	@RequestMapping("/updateReview")
+	public String updateReview(Model model, BookVO vo, ReviewVO rvo, HttpSession session) {
+		System.out.println("리뷰 컨트롤 수정 처리");
+		reviewservice.updateReview(rvo);
+		model.addAttribute("book", bookservice.getBook(vo));
+		model.addAttribute("review", reviewservice.getReviewList(rvo));
+		return "redirect:getBookList";
+	}
+
+	// 리뷰 삭제
+	@RequestMapping("deleteReview")
+	public String deleteReview(Model model, ReviewVO rvo) {
+		reviewservice.deleteReview(rvo);
+		return "redirect:getBookList";
+	}
+}
--- DDosProject/src/main/webapp/WEB-INF/views/book/updateReview.jsp
+++ DDosProject/src/main/webapp/WEB-INF/views/book/updateReview.jsp
@@ -1,18 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>리뷰 수정</title>
+<title>리뷰</title>
 </head>
 <body>
 	<div class="dashboard-main-wrapper">
 		<!-- ============================================================== -->
 		<!-- wrapper  -->
 		<!-- ============================================================== -->
-
 		<div class="dashboard-finance">
 			<div class="container-fluid dashboard-content">
 				<!-- ============================================================== -->
@@ -21,15 +19,15 @@
 				<div class="row">
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 						<div class="page-header">
-							<h3 class="mb-2">리뷰 수정</h3>
+							<h3 class="mb-2">Edit Review</h3>
 							<div class="page-breadcrumb">
 								<nav aria-label="breadcrumb">
 									<ol class="breadcrumb">
-										<li class="breadcrumb-item"><a href="#"
+										<li class="breadcrumb-item"><a href="adminHome"
 											class="breadcrumb-link">DDos</a></li>
-										<li class="breadcrumb-item active" aria-current="page">Review
-											Update</a>
-										</li>
+										<li class="breadcrumb-item"><a href="getReviewList"
+											class="breadcrumb-link">Review</a></li>
+										<li class="breadcrumb-item active" aria-current="page">Edit Review</li>
 									</ol>
 								</nav>
 							</div>
@@ -39,42 +37,49 @@
 				<!-- ============================================================== -->
 				<!-- end pageheader  -->
 				<!-- ============================================================== -->
-				<div class="row">
+				<div class="row" style="margin-top:2%;">
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-						<div class="section-block" id="basicform"></div>
 						<div class="card">
-							<form id="updateReview" action="updateReview" method="post" style="margin: 25px">
-									<div class="form-group">
-										<label for="inputText3" class="col-form-label">ISBN</label> <input
-											name="isbn" type="hidden" value="${review.isbn}">${review.isbn}</div>
-									<div class="form-group">
-										<label for="inputText3" class="col-form-label">리뷰 제목</label> <input
-											name="reviewTitle" type="text" class="form-control"
-											value="${review.reviewTitle}">${review.reviewTitle}
-									</div>
-									<div class="form-group">
-										<label for="exampleFormControlTextarea1">리뷰 내용</label>
-										<textarea class="form-control" name="reviewCont" rows="3">${review.reviewCont}</textarea>
-									</div>
-									<div class="form-group">
-										<label for="input-select">평점</label> 
-										<select class="form-control" name="reviewPoint" value="${review.reviewPoint}">
-											<option value="${review.reviewPoint}">${review.reviewPoint}</option>
-											<option value="1">1</option>
-											<option value="2">2</option>
-											<option value="3">3</option>
-											<option value="4">4</option>
-											<option value="5">5</option>
-										</select>
-									</div>
-
-									<div class="col-sm-6 pl-0">
-										<p class="text-right">
-											<button type="submit" class="btn btn-space btn-primary">저장</button>
-											<a href="javascript:history.back();">이전페이지로</a>
-										</p>
-									</div>
-							</form>
+							<div class="card-body">
+								<div class="table-responsive">
+									<table id="insertSuggestion" width="100%"
+										class="table table-bordered table-hover text-center">
+											<tr>
+											<td><input type="hidden" name="reviewNum" value="${review.reviewNum}">
+											<input type="hidden" name="memberId" value="${reveiw.memberId}"></td> 
+											</tr>
+											<tr>
+												<td>제목</td>
+												<td><input type="text" name="reviewTitle"
+													value="${review.reviewTitle}" style="width: 100%" required></td>
+											</tr>
+											<tr>
+												<td>내용</td>
+												<td><textarea rows="20" cols="20" name="reviewCont" style="width: 100%" required>${review.reviewCont}</textarea></td>
+											</tr>
+											<tr>
+												<td>날짜</td>
+												<td><input type="text" name="reviewDate" value="${review.reviewDate}" style="width:100%" required></td>
+											</tr>
+											<tr>
+												<td>별점</td>
+												<td><select	class="form-control" name="reviewPoint" value="${review.reviewPoint}">
+													<option value="${review.reviewPoint}">${review.reviewPoint}</option>
+													<option value="1">1</option>
+													<option value="2">2</option>
+													<option value="3">3</option>
+													<option value="4">4</option>
+													<option value="5">5</option>
+													</select>
+												</td>
+											<tr>
+												<td><input type="submit" class="btn btn-primary btn-sm"
+													style="width:20%; height:48px; font-weight:bold; " value="수정완료">
+												</td>
+											</tr>
+									</table>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>

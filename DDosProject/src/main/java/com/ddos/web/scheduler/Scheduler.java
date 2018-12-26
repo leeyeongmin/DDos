06a9d81f6525b0cc56df117e85d802fc55d7788d
User <User@YD03-15>
2018-12-26 10:56:53 +0900

    스케줄러

---
+++ DDosProject/src/main/java/com/ddos/web/scheduler/Scheduler.java
@@ -0,0 +1,24 @@
+package com.ddos.web.scheduler;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduler {
+	
+	@Autowired
+	private SqlSessionTemplate mybatis;
+	
+	@Scheduled(fixedDelay = 5000)		//좌석 시간 체크 후 시간이 완료되었으면 사용가능으로 변경 
+	public void seat_timeck() {
+		Date date = new Date();
+		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		mybatis.update("read.timecheck", format.format(date));
+	}
+
+}
